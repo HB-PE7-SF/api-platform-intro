59d8fb3a3656229808592133f14e9bf901a5f659
Lucas Delobelle <lucas@ld-web.net>
2023-12-06 09:59:03 +0100

    :sparkles: User Entity, Password hash subscriber

---
+++ src/EventSubscriber/HashUserPasswordSubscriber.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Entity\User;
+use Doctrine\Bundle\DoctrineBundle\EventSubscriber\EventSubscriberInterface;
+use Doctrine\ORM\Event\PrePersistEventArgs;
+use Doctrine\ORM\Events;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class HashUserPasswordSubscriber implements EventSubscriberInterface
+{
+    public function __construct(
+        private UserPasswordHasherInterface $hasher
+    ) {
+    }
+
+    public function prePersist(PrePersistEventArgs $args): void
+    {
+        // L'entité qui va être persistée
+        $object = $args->getObject();
+
+        if (!$object instanceof User) {
+            return;
+        }
+
+        $userPassword = $object->getPassword();
+
+        $object->setPassword(
+            $this->hasher->hashPassword(
+                $object,
+                $userPassword
+            )
+        );
+    }
+
+    public function getSubscribedEvents()
+    {
+        return [
+            Events::prePersist
+        ];
+    }
+}
